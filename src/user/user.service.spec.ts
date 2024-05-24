36f7ad6dd7d53e6ba6e25ff3f60db1f99cec496c
knowave <kwaktaemin98@gmail.com>
2024-05-24 15:00:47 +0900

    user unit test 정의

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { UserRepository } from './repo/user.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+
+const mockRepository = () => ({
+  save: jest.fn(),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof UserRepository, jest.Mock>>;
 
 describe('UserService', () => {
   let service: UserService;
+  let repository: MockRepository<UserRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: UserRepository,
+          useValue: mockRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    repository = module.get<MockRepository<UserRepository>>(UserRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('create user', () => {
+    it('should create a user', async () => {
+      const createUser: CreateUserDto = {
+        email: 'test@test.com',
+        username: 'test',
+        nickname: 'test',
+        password: 'test1234!',
+        phoneNumber: '010-1234-5678',
+        introduction: 'tester',
+      };
+
+      repository.save.mockResolvedValue(createUser);
+      const result = await service.createUser(createUser);
+      expect(repository.save).toHaveBeenCalledWith(expect.any(createUser));
+      expect(result).toEqual(createUser);
+    });
   });
 });
