5cb12db196430656d6572c915f475c4608ec8761
knowave <kwaktaemin98@gmail.com>
2024-05-24 14:39:46 +0900

    userRepositoryModule 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UserRepositoryModule } from './repo/user.repository.module';
 
 @Module({
+  imports: [UserRepositoryModule],
   controllers: [UserController],
   providers: [UserService],
 })
