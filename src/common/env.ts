024dcfd1f4a7b5689133b7e2604481914277faba
knowave <kwaktaemin98@gmail.com>
2024-05-24 15:47:17 +0900

    add jwt env variables

--- src/common/env.ts
+++ src/common/env.ts
@@ -6,3 +6,11 @@ export const DATABASE_PORT = +process.env.DATABASE_PORT;
 export const DATABASE_USER = process.env.DATABASE_USER;
 export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 export const DATABASE_NAME = process.env.DATABASE_NAME;
+
+// JWT Environment Variables
+export const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET;
+export const JWT_ACCESS_TOKEN_EXPIRATION_TIME =
+  process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME;
+export const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET;
+export const JWT_REFRESH_TOKEN_EXPIRATION_TIME =
+  process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME;
