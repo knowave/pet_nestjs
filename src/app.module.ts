d18f4b0f6bd7d01af3f31fd98a9732be32236fb9
knowave <kwaktaemin98@gmail.com>
2024-05-24 17:21:14 +0900

    ADD FEED

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { FeedModule } from './feed/feed.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
     MysqlModule,
     UserModule,
     AuthModule,
+    FeedModule,
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: JwtAuthGuard }],
