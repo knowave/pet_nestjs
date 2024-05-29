a67887757da160286bd6a2605ac4bab5440a7c13
knowave <kwaktaemin98@gmail.com>
2024-05-29 11:01:26 +0900

    feed 생성과 내가 작성한 feed 조회 unit test

--- src/feed/feed.service.spec.ts
+++ src/feed/feed.service.spec.ts
@@ -1,18 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FeedService } from './feed.service';
+import { FeedRepository } from './repo/feed.repository';
+import { User } from 'src/user/entities/user.entity';
+import { CreateFeedDto } from './dto/create-feed.dto';
+import { Feed } from './entities/feed.entity';
+import { NotFoundException } from '@nestjs/common';
+import { FEED_NOT_FOUND } from './error/feed.error';
+
+const mockRepository = () => ({
+  save: jest.fn(),
+  getFeedByFeedIdAndUserId: jest.fn(),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof FeedRepository, jest.Mock>>;
 
 describe('FeedService', () => {
   let service: FeedService;
+  let repository: MockRepository<FeedRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FeedService],
+      providers: [
+        FeedService,
+        {
+          provide: FeedRepository,
+          useValue: mockRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<FeedService>(FeedService);
+    repository = module.get<MockRepository<FeedRepository>>(FeedRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('create feed', () => {
+    it('Feed를 생성한다.', async () => {
+      const user = new User({ id: 'uuid', email: 'test@test.com' });
+      const feed: CreateFeedDto = {
+        content: 'test content',
+        title: 'test title',
+        thumbnail: 'test thumbnail',
+      };
+
+      repository.save.mockResolvedValue(feed);
+      const result = await service.createFeed(feed, user);
+
+      expect(result).toEqual(true);
+    });
+  });
+
+  describe('get my feed', () => {
+    it('내가 작성한 Feed를 조회한다.', async () => {
+      const user = new User({ id: 'uuid_1', email: 'test@test.com' });
+      const feed = new Feed({
+        id: 'feed_uuid',
+        content: 'test content',
+        title: 'test title',
+        user,
+      });
+
+      repository.getFeedByFeedIdAndUserId.mockResolvedValue(feed);
+      const result = await service.getMyFeed(feed.id, user.id);
+
+      expect(result.id).toEqual(feed.id);
+      expect(result.user).toEqual(user);
+    });
+
+    it('Feed를 찾을 수 없는 경우 에러를 반환한다.', async () => {
+      const user = new User({ id: 'uuid_1', email: 'test@test.com' });
+      const feedId = 'feed_uuid';
+
+      repository.getFeedByFeedIdAndUserId.mockResolvedValue(undefined);
+
+      await expect(service.getMyFeed(feedId, user.id)).rejects.toEqual(
+        expect.objectContaining({
+          status: 404,
+          response: {
+            code: FEED_NOT_FOUND.code,
+            message: FEED_NOT_FOUND.message,
+          },
+        }),
+      );
+    });
   });
 });
--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -1,19 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateFeedDto } from './dto/create-feed.dto';
 import { UpdateFeedDto } from './dto/update-feed.dto';
+import { User } from 'src/user/entities/user.entity';
+import { FeedRepository } from './repo/feed.repository';
+import { Feed } from './entities/feed.entity';
+import { FEED_NOT_FOUND } from './error/feed.error';
 
 @Injectable()
 export class FeedService {
-  create(createFeedDto: CreateFeedDto) {
-    return 'This action adds a new feed';
+  constructor(private readonly feedRepository: FeedRepository) {}
+
+  async createFeed(createFeedDto: CreateFeedDto, user: User): Promise<boolean> {
+    const { content, title, thumbnail } = createFeedDto;
+
+    await this.feedRepository.save(
+      new Feed({
+        content,
+        title,
+        thumbnail,
+        user,
+      }),
+    );
+    return true;
   }
 
   findAll() {
     return `This action returns all feed`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} feed`;
+  async getMyFeed(feedId: string, userId: string): Promise<Feed> {
+    const feed = await this.feedRepository.getFeedByFeedIdAndUserId(
+      feedId,
+      userId,
+    );
+
+    if (!feed) throw new NotFoundException(FEED_NOT_FOUND);
+
+    return feed;
   }
 
   update(id: number, updateFeedDto: UpdateFeedDto) {
