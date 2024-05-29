9f712bd23a2ec0572e60e5ca723a5fda888b9761
knowave <kwaktaemin98@gmail.com>
2024-05-29 11:00:56 +0900

    생성 및 단건 조회 feed 수정

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -1,25 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { FeedService } from './feed.service';
 import { CreateFeedDto } from './dto/create-feed.dto';
 import { UpdateFeedDto } from './dto/update-feed.dto';
+import { CurrentUser } from 'src/user/user.decorator';
+import { User } from 'src/user/entities/user.entity';
 
 @Controller('feed')
 export class FeedController {
   constructor(private readonly feedService: FeedService) {}
 
   @Post()
-  create(@Body() createFeedDto: CreateFeedDto) {
-    return this.feedService.create(createFeedDto);
+  async createFeed(
+    @Body() createFeedDto: CreateFeedDto,
+    @CurrentUser() user: User,
+  ) {
+    return await this.feedService.createFeed(createFeedDto, user);
+  }
+
+  @Get('/my/feed/:feedId')
+  async findOne(@Param('feedId') feedId: string, @CurrentUser() user: User) {
+    return await this.feedService.getMyFeed(feedId, user.id);
   }
 
   @Get()
   findAll() {
     return this.feedService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.feedService.findOne(+id);
   }
 
   @Patch(':id')
