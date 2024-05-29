f4eeb60098a6153fdd22a75aefd44ff4823780ba
knowave <kwaktaemin98@gmail.com>
2024-05-29 10:28:38 +0900

    feed repository

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FeedService } from './feed.service';
 import { FeedController } from './feed.controller';
+import { FeedRepositoryModule } from './repo/feed.repository.module';
 
 @Module({
+  imports: [FeedRepositoryModule],
   controllers: [FeedController],
   providers: [FeedService],
 })
