259487d1681741b58b5d84d36bf4e2307b8dd57b
knowave <kwaktaemin98@gmail.com>
2024-05-29 10:59:51 +0900

    create feed dto pick column

--- src/feed/dto/create-feed.dto.ts
+++ src/feed/dto/create-feed.dto.ts
@@ -1,1 +1,8 @@
-export class CreateFeedDto {}
+import { PickType } from '@nestjs/mapped-types';
+import { Feed } from '../entities/feed.entity';
+
+export class CreateFeedDto extends PickType(Feed, [
+  'content',
+  'thumbnail',
+  'title',
+]) {}
