d43c2648b56d90470c7ca24b07614d77026b7456
knowave <kwaktaemin98@gmail.com>
2024-05-30 13:04:05 +0900

    follwer, following list 조회 메서드

--- src/follow/repo/follow.repository.ts
+++ src/follow/repo/follow.repository.ts
@@ -7,14 +7,32 @@ import { Repository } from 'typeorm';
 export class FollowRepository {
   constructor(
     @InjectRepository(Follow)
-    private readonly followRepository: Repository<Follow>,
+    private readonly repository: Repository<Follow>,
   ) {}
 
   async save(follow: Follow): Promise<Follow> {
-    return this.followRepository.save(follow);
+    return await this.repository.save(follow);
   }
 
   async bulkSave(follows: Follow[]): Promise<Follow[]> {
-    return this.followRepository.save(follows);
+    return await this.repository.save(follows);
+  }
+
+  async getFollowers(followingId: number): Promise<Follow[]> {
+    return await this.repository
+      .createQueryBuilder('follow')
+      .innerJoinAndSelect('follow.follower', 'follower')
+      .innerJoinAndSelect('follower.user', 'user')
+      .where('follow.followingId = :followingId', { followingId })
+      .getMany();
+  }
+
+  async getFollowings(followerId: number): Promise<Follow[]> {
+    return await this.repository
+      .createQueryBuilder('follow')
+      .innerJoinAndSelect('follow.following', 'following')
+      .innerJoinAndSelect('following.user', 'user')
+      .where('follow.followerId = :followerId', { followerId })
+      .getMany();
   }
 }
