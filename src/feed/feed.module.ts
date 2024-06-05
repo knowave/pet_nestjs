80294fc17d4f4ba5e056459b0f0333114a10e95b
knowave <kwaktaemin98@gmail.com>
2024-06-05 17:35:37 +0900

    Add S3Module

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -3,9 +3,10 @@ import { FeedService } from './feed.service';
 import { FeedController } from './feed.controller';
 import { FeedRepositoryModule } from './repo/feed.repository.module';
 import { RedisConfigModule } from 'src/database/redis/redis.config.module';
+import { S3Module } from 'src/s3/s3.module';
 
 @Module({
-  imports: [FeedRepositoryModule, RedisConfigModule],
+  imports: [FeedRepositoryModule, RedisConfigModule, S3Module],
   controllers: [FeedController],
   providers: [FeedService],
 })
