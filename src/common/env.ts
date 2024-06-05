7d52e59de555195a2a7c89d6344c0bed99efcb1f
knowave <kwaktaemin98@gmail.com>
2024-06-05 11:37:09 +0900

    add redis host and port

--- src/common/env.ts
+++ src/common/env.ts
@@ -23,3 +23,7 @@ export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 export const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
 export const AWS_REGION = process.env.AWS_REGION;
+
+// REDIS Environment Variables
+export const REDIS_HOST = process.env.REDIS_HOST;
+export const REDIS_PORT = process.env.REDIS_PORT;
