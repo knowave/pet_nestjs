80ab35fa82697af8c0e317405257b79935f4e452
knowave <kwaktaemin98@gmail.com>
2024-06-05 14:33:47 +0900

    Redis mocking

--- src/feed/feed.service.spec.ts
+++ src/feed/feed.service.spec.ts
@@ -6,19 +6,30 @@ import { CreateFeedDto } from './dto/create-feed.dto';
 import { Feed } from './entities/feed.entity';
 import { FEED_NOT_FOUND } from './error/feed.error';
 import { NotFoundException } from '@nestjs/common';
+import { faker } from '@faker-js/faker';
+import { RedisService } from 'src/database/redis/redis.service';
 
 const mockRepository = () => ({
   save: jest.fn(),
   getFeedByFeedIdAndUserId: jest.fn(),
   getFeedByFeedIdAndIsPublic: jest.fn(),
   increment: jest.fn(),
+  getTopTenFeedsAndSortViewCount: jest.fn(),
+});
+
+const mockRedisService = () => ({
+  get: jest.fn(),
+  set: jest.fn(),
+  del: jest.fn(),
 });
 
 type MockRepository<T = any> = Partial<Record<keyof FeedRepository, jest.Mock>>;
+type MockRedisService<T = any> = Partial<Record<keyof RedisService, jest.Mock>>;
 
 describe('FeedService', () => {
   let service: FeedService;
   let repository: MockRepository<FeedRepository>;
+  let redisService: MockRedisService<RedisService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -28,11 +39,16 @@ describe('FeedService', () => {
           provide: FeedRepository,
           useValue: mockRepository(),
         },
+        {
+          provide: RedisService,
+          useValue: mockRedisService(),
+        },
       ],
     }).compile();
 
     service = module.get<FeedService>(FeedService);
     repository = module.get<MockRepository<FeedRepository>>(FeedRepository);
+    redisService = module.get<MockRedisService<RedisService>>(RedisService);
   });
 
   afterEach(() => {
@@ -162,6 +178,34 @@ describe('FeedService', () => {
           },
         }),
       );
+    });
+  });
+
+  describe('top ten feeds', () => {
+    it('조회수가 높은 상위 10개의 Feed를 조회한다.', async () => {
+      const viewCounts = Array.from(
+        { length: 10 },
+        (_, i) => (i + 1) * 10,
+      ).sort(() => Math.random() - 0.5);
+
+      const feeds = viewCounts.map((viewCount) => {
+        return new Feed({
+          id: faker.string.uuid(),
+          title: faker.lorem.sentence(),
+          content: faker.lorem.paragraph(),
+          viewCount,
+        });
+      });
+
+      repository.getTopTenFeedsAndSortViewCount.mockResolvedValue(
+        feeds.sort((a, b) => b.viewCount - a.viewCount),
+      );
+      redisService.get.mockResolvedValue(JSON.stringify(feeds));
+      redisService.set.mockResolvedValue(JSON.stringify(feeds));
+
+      const result = await service.topTenFeeds();
+
+      expect(result[0]).toEqual(feeds[0]);
     });
   });
 });
