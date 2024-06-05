cc697980b31f068c7c44b3f85649d237fb096ece
knowave <kwaktaemin98@gmail.com>
2024-06-05 17:36:59 +0900

    thumbanil type is uploadFileDto

--- src/feed/dto/update-feed.dto.ts
+++ src/feed/dto/update-feed.dto.ts
@@ -1,9 +1,11 @@
 import { PickType } from '@nestjs/mapped-types';
 import { Feed } from '../entities/feed.entity';
+import { UploadFileDto } from 'src/common/upload-file-dto';
 
 export class UpdateFeedDto extends PickType(Feed, [
   'content',
   'title',
-  'thumbnail',
   'isPublic',
-]) {}
+]) {
+  thumbnail: UploadFileDto;
+}
