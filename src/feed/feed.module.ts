b2d73191ca612ee054e4573c4346eda7d58d5323
knowave <kwaktaemin98@gmail.com>
2024-06-05 14:34:24 +0900

    Add RedisConfigModule

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FeedService } from './feed.service';
 import { FeedController } from './feed.controller';
 import { FeedRepositoryModule } from './repo/feed.repository.module';
+import { RedisConfigModule } from 'src/database/redis/redis.config.module';
 
 @Module({
-  imports: [FeedRepositoryModule],
+  imports: [FeedRepositoryModule, RedisConfigModule],
   controllers: [FeedController],
   providers: [FeedService],
 })
