76aefbf80bc147220a808947abc73ec8ccb0bed9
knowave <kwaktaemin98@gmail.com>
2024-06-12 10:10:45 +0900

    s3Module 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepositoryModule } from './repo/user.repository.module';
+import { S3Module } from 'src/s3/s3.module';
 
 @Module({
-  imports: [UserRepositoryModule],
+  imports: [UserRepositoryModule, S3Module],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
