a6bb269264c3389750f6387b5a51e3a573b856b6
knowave <kwaktaemin98@gmail.com>
2024-06-20 15:19:00 +0900

    feed repository module, comment repository module 추가

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import { FeedRepositoryModule } from 'src/feed/repo/feed.repository.module';
+import { CommentRepositoryModule } from './repo/comment.repository.module';
 
 @Module({
+  imports: [CommentRepositoryModule, FeedRepositoryModule],
   controllers: [CommentController],
   providers: [CommentService],
 })
