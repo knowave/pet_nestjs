52f788cccafafa42be278c797b386ffee8edc6f9
knowave <kwaktaemin98@gmail.com>
2024-06-20 17:09:50 +0900

    comment 생성 unit test 진행중

--- src/comment/comment.service.spec.ts
+++ src/comment/comment.service.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentService } from './comment.service';
+import { FeedRepository } from 'src/feed/repo/feed.repository';
+import { CommentRepository } from './repo/comment.repository';
+
+const feedMockRepository = () => ({
+  getFeedByFeedIdAndIsPublic: jest.fn(),
+});
+
+const commentMockRepository = () => ({
+  save: jest.fn(),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof T, jest.Mock>>;
 
 describe('CommentService', () => {
   let service: CommentService;
+  let feedRepository: MockRepository<FeedRepository>;
+  let commentRepository: MockRepository<CommentRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CommentService],
+      providers: [
+        CommentService,
+        {
+          provide: FeedRepository,
+          useValue: feedMockRepository(),
+        },
+        {
+          provide: CommentRepository,
+          useValue: commentMockRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<CommentService>(CommentService);
+    feedRepository = module.get<MockRepository<FeedRepository>>(FeedRepository);
+    commentRepository =
+      module.get<MockRepository<CommentRepository>>(CommentRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createComment', () => {
+    it('feed가 존재하면 comment를 생성하고 true를 반환한다.', async () => {
+      const user = { id: 'uuid1', username: 'username' };
+      const feedId = 'uuid2';
+      const comment = 'comment';
+
+      feedRepository.getFeedByFeedIdAndIsPublic.mockResolvedValue({
+        feedId,
+      });
+      commentRepository.save.mockResolvedValue(comment);
+    });
   });
 });
