1ec81d742153537c9207ddbb2ef92d5c2022d5ba
knowave <kwaktaemin98@gmail.com>
2024-06-20 15:21:38 +0900

    create comment controller

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,34 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Param } from '@nestjs/common';
 import { CommentService } from './comment.service';
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
+import { CurrentUser } from 'src/common/decorators/user.decorator';
+import { User } from 'src/user/entities/user.entity';
 
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
-  @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
-    return this.commentService.create(createCommentDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.commentService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commentService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
-    return this.commentService.update(+id, updateCommentDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.commentService.remove(+id);
+  @Post('/:feedId')
+  async createComment(
+    @Param('feedId') feedId: string,
+    @Body('comment') comment: string,
+    @CurrentUser() user: User,
+  ): Promise<boolean> {
+    return await this.commentService.createComment(user, comment, feedId);
   }
 }
