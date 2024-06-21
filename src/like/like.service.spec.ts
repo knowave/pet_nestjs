5b6d51381c92c9d2d6c486c79f47557463635546
knowave <kwaktaemin98@gmail.com>
2024-06-21 16:49:11 +0900

    feed 좋아요 단위 테스트

--- src/like/like.service.spec.ts
+++ src/like/like.service.spec.ts
@@ -1,18 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LikeService } from './like.service';
+import { LikeRepository } from './repo/like.repository';
+import { FeedRepository } from 'src/feed/repo/feed.repository';
+import { Feed } from 'src/feed/entities/feed.entity';
+import { User } from 'src/user/entities/user.entity';
+import { CommentRepository } from 'src/comment/repo/comment.repository';
+
+const likeMockRepository = () => ({
+  save: jest.fn(),
+  softRemove: jest.fn(),
+  getLikeByFeedIdAndUserId: jest.fn(),
+});
+
+const feedMockRepository = () => ({
+  getFeedById: jest.fn(),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof T, jest.Mock>>;
 
 describe('LikeService', () => {
   let service: LikeService;
+  let likeRepository: MockRepository<LikeRepository>;
+  let feedRepository: MockRepository<FeedRepository>;
+  let commentRepository: MockRepository<CommentRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LikeService],
+      providers: [
+        LikeService,
+        {
+          provide: LikeRepository,
+          useValue: likeMockRepository(),
+        },
+        {
+          provide: FeedRepository,
+          useValue: feedMockRepository(),
+        },
+        {
+          provide: CommentRepository,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<LikeService>(LikeService);
+    likeRepository = module.get<MockRepository<LikeRepository>>(LikeRepository);
+    feedRepository = module.get<MockRepository<FeedRepository>>(FeedRepository);
+    commentRepository =
+      module.get<MockRepository<CommentRepository>>(CommentRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('feedLike', () => {
+    it('feed가 존재하고, 해당 user가 이미 feed를 좋아요를 누르지 않은 경우 true를 반환한다.', async () => {
+      const feed = new Feed({
+        id: 'feedId',
+        content: 'content',
+      });
+      const user = new User({
+        id: 'userId',
+        username: 'username',
+      });
+
+      feedRepository.getFeedById.mockResolvedValue(feed);
+      likeRepository.getLikeByFeedIdAndUserId.mockResolvedValue(null);
+
+      const result = await service.feedLike(feed.id, user);
+
+      expect(result).toBe(true);
+    });
+
+    it('feed가 존재하고, 해당 user가 이미 feed를 좋아요를 눌렀을 경우 false를 반환한다.', async () => {
+      const feed = new Feed({
+        id: 'feedId',
+        content: 'content',
+      });
+      const user = new User({
+        id: 'userId',
+        username: 'username',
+      });
+
+      feedRepository.getFeedById.mockResolvedValue(feed);
+      likeRepository.getLikeByFeedIdAndUserId.mockResolvedValue({ feed, user });
+
+      const result = await service.feedLike(feed.id, user);
+
+      expect(result).toBe(false);
+    });
   });
 });
