ff66491a8019473b95fb2c771b752708f64581d3
knowave <kwaktaemin98@gmail.com>
2024-06-21 16:02:28 +0900

    generate pick module

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { FeedModule } from './feed/feed.module';
 import { CommentModule } from './comment/comment.module';
 import { FollowModule } from './follow/follow.module';
 import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { PickModule } from './pick/pick.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { RedisModule } from '@liaoliaots/nestjs-redis';
     FeedModule,
     CommentModule,
     FollowModule,
+    PickModule,
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: JwtAuthGuard }],
