7507e60f0938bb65d49b31a0af18a09976d5e7af
knowave <kwaktaemin98@gmail.com>
2024-06-21 16:15:40 +0900

    Add Like entity entity decorator

--- src/like/entities/like.entity.ts
+++ src/like/entities/like.entity.ts
@@ -2,8 +2,9 @@ import { Comment } from 'src/comment/entities/comment.entity';
 import { BaseEntity } from 'src/common/base.entity';
 import { Feed } from 'src/feed/entities/feed.entity';
 import { User } from 'src/user/entities/user.entity';
-import { ManyToOne } from 'typeorm';
+import { Entity, ManyToOne } from 'typeorm';
 
+@Entity()
 export class Like extends BaseEntity {
   @ManyToOne(() => User, (user) => user.likes, {
     onDelete: 'CASCADE',
