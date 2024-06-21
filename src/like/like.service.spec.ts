8a2ea281a1906ac17b399d7a325d28715d2f4ad3
knowave <kwaktaemin98@gmail.com>
2024-06-21 16:59:40 +0900

    comment 좋아요

--- src/like/like.service.spec.ts
+++ src/like/like.service.spec.ts
@@ -4,16 +4,22 @@ import { LikeRepository } from './repo/like.repository';
 import { FeedRepository } from 'src/feed/repo/feed.repository';
 import { Feed } from 'src/feed/entities/feed.entity';
 import { User } from 'src/user/entities/user.entity';
+import { Comment } from 'src/comment/entities/comment.entity';
 import { CommentRepository } from 'src/comment/repo/comment.repository';
 
 const likeMockRepository = () => ({
   save: jest.fn(),
   softRemove: jest.fn(),
   getLikeByFeedIdAndUserId: jest.fn(),
+  getLikeByCommentIdAndUserId: jest.fn(),
 });
 
 const feedMockRepository = () => ({
   getFeedById: jest.fn(),
+});
+
+const commentMockRepository = () => ({
+  getCommentById: jest.fn(),
 });
 
 type MockRepository<T = any> = Partial<Record<keyof T, jest.Mock>>;
@@ -38,7 +44,7 @@ describe('LikeService', () => {
         },
         {
           provide: CommentRepository,
-          useValue: {},
+          useValue: commentMockRepository(),
         },
       ],
     }).compile();
@@ -83,6 +89,47 @@ describe('LikeService', () => {
       likeRepository.getLikeByFeedIdAndUserId.mockResolvedValue({ feed, user });
 
       const result = await service.feedLike(feed.id, user);
+
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('commentLike', () => {
+    it('comment가 존재하고, 해당 user가 이미 comment를 좋아요를 누르지 않은 경우 true를 반환한다.', async () => {
+      const comment = new Comment({
+        id: 'commentId',
+        content: 'content',
+      });
+      const user = new User({
+        id: 'userId',
+        username: 'username',
+      });
+
+      commentRepository.getCommentById.mockResolvedValue(comment);
+      likeRepository.getLikeByCommentIdAndUserId.mockResolvedValue(null);
+
+      const result = await service.commentLike(comment.id, user);
+
+      expect(result).toBe(true);
+    });
+
+    it('comment가 존재하고, 해당 user가 이미 comment를 좋아요를 눌렀을 경우 false를 반환한다.', async () => {
+      const comment = new Comment({
+        id: 'commentId',
+        content: 'content',
+      });
+      const user = new User({
+        id: 'userId',
+        username: 'username',
+      });
+
+      commentRepository.getCommentById.mockResolvedValue(comment);
+      likeRepository.getLikeByCommentIdAndUserId.mockResolvedValue({
+        comment,
+        user,
+      });
+
+      const result = await service.commentLike(comment.id, user);
 
       expect(result).toBe(false);
     });
