bf431d0b99c547584d79fcce919f857ecef1316a
knowave <kwaktaemin98@gmail.com>
2024-06-21 16:38:00 +0900

    import feed, comment repository

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { LikeService as LikeService } from './like.service';
 import { LikeController } from './like.controller';
 import { LikeRepositoryModule } from './repo/like.repository.module';
+import { FeedRepositoryModule } from 'src/feed/repo/feed.repository.module';
+import { CommentRepositoryModule } from 'src/comment/repo/comment.repository.module';
 
 @Module({
-  imports: [LikeRepositoryModule],
+  imports: [
+    LikeRepositoryModule,
+    FeedRepositoryModule,
+    CommentRepositoryModule,
+  ],
   controllers: [LikeController],
   providers: [LikeService],
 })
