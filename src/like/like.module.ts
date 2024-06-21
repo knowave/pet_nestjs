43662ecb320b7b8b274341c1d8f77a32d4c9c18d
knowave <kwaktaemin98@gmail.com>
2024-06-21 16:29:36 +0900

    import like repository module

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LikeService as LikeService } from './like.service';
 import { LikeController } from './like.controller';
+import { LikeRepositoryModule } from './repo/like.repository.module';
 
 @Module({
+  imports: [LikeRepositoryModule],
   controllers: [LikeController],
   providers: [LikeService],
 })
